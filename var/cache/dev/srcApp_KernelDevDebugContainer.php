a3e32273113c4d3f1d765d441a95304fc6d049db
jmpazrod <jmpazrod@gmail.com>
2019-09-17 13:56:06 -0500

    ajustes gitignore

--- var/cache/dev/srcApp_KernelDevDebugContainer.php
+++ var/cache/dev/srcApp_KernelDevDebugContainer.php
@@ -16,6 +16,6 @@ if (!\class_exists(srcApp_KernelDevDebugContainer::class, false)) {
 
 return new \ContainerVq6QSOe\srcApp_KernelDevDebugContainer([
     'container.build_hash' => 'Vq6QSOe',
-    'container.build_id' => 'd9f45776',
-    'container.build_time' => 1568704512,
+    'container.build_id' => '63f88f78',
+    'container.build_time' => 1568746242,
 ], __DIR__.\DIRECTORY_SEPARATOR.'ContainerVq6QSOe');
